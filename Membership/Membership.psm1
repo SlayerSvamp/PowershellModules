83df04567e012e63a3f8f1326bab6313520263f1
SlayerSvamp <linus.aspelin@gmail.com>
2018-05-18 10:45:01 +0200

    addning new and renamed version

---
+++ Membership/Membership.psm1
@@ -0,0 +1,51 @@
+﻿#powerkjell-skript skrivet i powerkällaren... powerhouse
+function Get-Membership {
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [string[]]$SamAccountName,
+        [string]$Filter,
+        [string]$Ignore
+    )
+
+    begin {
+        $names = New-Object System.Collections.Generic.List[System.String]
+    }
+    process {
+        $names.AddRange($SamAccountName)
+    }
+    end {
+        $ldapFilter = "(|$( -join ($names | ForEach-Object { "(samaccountname=$_)"})))"
+        $splat = @{
+            Properties = 'MemberOf', 'SamAccountName'
+            LDAPFilter = $ldapFilter
+        }
+
+        Get-ADObject @splat |
+            Foreach-Object {
+                $user = $_.SamAccountName
+                $_.MemberOf | 
+                    ForEach-Object {
+                        $group = ($_ -split '=|,')[1]
+                        $isIgnored = $Ignore -and ($group -match $Ignore)
+                        $isFiltered = $group -match $Filter
+                        if($isFiltered -and -not $isIgnored) {
+                            [PSCustomObject]@{
+                                User     = $user
+                                MemberOf = $group
+                            }
+                        }
+                    }
+            } |
+            Group-Object -Property MemberOf |
+                ForEach-Object {
+                    [PSCustomObject] @{
+                        Count = $_.Count
+                        Member = $_.Group.User
+                        Group = $_.Name
+                    }
+                } |
+                Sort-Object -Property Count, Member, Group
+    }
+}
+
+Export-ModuleMember '*-*'
