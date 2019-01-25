3e35565bbe5c03c68f0bc90a897ca1a65272edc4
Linus Aspelin <linus.aspelin@trafikverket.se>
2019-01-25 14:27:28 +0100

    included export script, changed value eq to compare with more info

--- Compare/Compare.psm1
+++ Compare/Compare.psm1
@@ -13,26 +13,39 @@
         [ValidateSet('MACTripleDES', 'MD5', 'RIPEMD160', 'SHA1', 'SHA256', 'SHA384', 'SHA512')]
         [string]$Algorithm = 'MD5'
     )
-
-    $gciParam = @{
-        File        = $true
-        Recurse     = $true
-        ErrorAction = 'SilentlyContinue'
+    begin {
+        $gciParam = @{
+            File        = $true
+            Recurse     = $true
+        }
     }
-    
-    #todo: make function
-    $firstFileHash = @{}
-    Get-ChildItem @gciParam -Path $First | ForEach-Object { $name = $_.FullName -replace [regex]::Escape((Resolve-Path $First).Path), '.'; $firstFileHash[$name] = (Get-FileHash -Path $_.FullName -Algorithm $Algorithm).Hash }
-    $secondFileHash = @{}
-    Get-ChildItem @gciParam -Path $Second | ForEach-Object { $name = $_.FullName -replace [regex]::Escape((Resolve-Path $Second).Path), '.'; $secondFileHash[$name] = (Get-FileHash -Path $_.FullName -Algorithm $Algorithm).Hash }
-
-    $uniqueFilenames = ($firstFileHash.Keys + $secondFileHash.Keys) | select -Unique
-    $uniqueFilenames | ForEach-Object {
-        [PSCustomObject]@{
-            Name   = $_
-            First  = $firstFileHash[$_]
-            Second = $secondFileHash[$_]
-            Eq     = $firstFileHash[$_] -eq $secondFileHash[$_]
+    process {
+        #todo: make function
+        $firstFileHash = @{}
+        Get-ChildItem @gciParam -Path $First | ForEach-Object { $name = $_.FullName -replace [regex]::Escape((Resolve-Path $First).Path), '.'; $firstFileHash[$name] = (Get-FileHash -Path $_.FullName -Algorithm $Algorithm).Hash }
+        $secondFileHash = @{}
+        Get-ChildItem @gciParam -Path $Second | ForEach-Object { $name = $_.FullName -replace [regex]::Escape((Resolve-Path $Second).Path), '.'; $secondFileHash[$name] = (Get-FileHash -Path $_.FullName -Algorithm $Algorithm).Hash }
+    }
+    end {
+        $uniqueFilenames = ($firstFileHash.Keys + $secondFileHash.Keys) | select -Unique
+        $uniqueFilenames | ForEach-Object {
+            [PSCustomObject]@{
+                Name    = $_
+                First   = $firstFileHash[$_]
+                Second  = $secondFileHash[$_]
+                Compare = $(
+                    if($firstFileHash[$_]) {
+                        if($secondFileHash[$_]) {
+                            if($firstFileHash[$_] -eq $secondFileHash[$_]){ 'Equal' }
+                            else { 'Not equal' }
+                        }
+                        else { 'Only first' }
+                    }
+                    else { 'Only second' }
+                )
+            }
         }
     }
 }
+
+Export-ModuleMember '*-*'
